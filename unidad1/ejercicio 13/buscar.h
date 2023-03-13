e6a73054c93f71d2e5405c6cf87574dd1f783bb7
Guidotss <guidoolguin@hotmail.com>
2023-03-13 20:25:41 -0300

    agregando ejercicio 15

--- unidad1/ejercicio 13/buscar.h
+++ unidad1/ejercicio 13/buscar.h
@@ -1,10 +1,11 @@
 
-auto  buscarIndex(std::vector<int>&v, int k){
+
+int buscarIndex(std::vector<int>&v,k){
 
     for(int i = 0; i < v.size(); i++){
-        if(v[i] == k){
-            return i;
-        }
+       if(v[i] == k){
+           return i;
+       }
     }
 
     return -1;
