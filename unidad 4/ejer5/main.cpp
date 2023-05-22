f578297a765bc2252ecb0e5a12f6fe82d6adde3c
Guidotss <guidoolguin@hotmail.com>
2023-05-21 21:21:33 -0300

    Terminando clase clerigo

--- unidad 4/ejer5/main.cpp
+++ unidad 4/ejer5/main.cpp
@@ -3,13 +3,15 @@
 #include"menu/menu.h"
 #include"./personaje/personaje.h"
 #include"./personaje/mago/mago.h"
+#include"./personaje/clerigo/clerigo.h"
 
 
 int main(){
-    std::unique_ptr<Personaje> personaje = std::make_unique<Mago>(); 
+    std::unique_ptr<Personaje> mago1 = std::make_unique<Mago>(); 
+    std::unique_ptr<Personaje> clerigo1 = std::make_unique<Clerigo>();
+    std::unique_ptr<Personaje> mago2 = std::make_unique<Mago>();
+    std::unique_ptr<Personaje> clerigo2 = std::make_unique<Clerigo>();
 
-    personaje->Set_nombre("Gandalf");
-    personaje->Set_raza("Humano");
 
 
 
